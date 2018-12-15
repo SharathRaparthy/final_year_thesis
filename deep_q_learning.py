26a40a7e6789dfdf005f714fced8ba496073e713
SharathRaparthy <sharathchandra.raparthy@gmail.com>
2018-12-15 12:02:03 +0530

    code ready

--- deep_q_learning.py
+++ deep_q_learning.py
@@ -44,6 +44,7 @@ def stack_images(stacked_frames, state, new_episode):
         stacked_frames.append(frame)
 
         stacked_state = np.stack(stacked_frames, axis = 0)
+        print(stacked_state.shape)
     else:
         stacked_frames.append(frame)
         stacked_state = np.stack(stacked_frames, axis = 0)
@@ -67,7 +68,7 @@ memory_size = 1000000
 #build network architecture
 class DeepQNet(nn.Module):
     """docstring forDeepQNet."""
-    def __init__(self, state_size, action_size = 3):
+    def __init__(self, state_size, action_size = action_size):
         super(DeepQNet, self).__init__()
         self.conv1 = nn.Conv2d(4,32,8,stride = 4)
         self.conv2 = nn.Conv2d(32,64,4,stride = 2)
@@ -89,11 +90,8 @@ class DeepQNet(nn.Module):
     def act(self,state, epsilon):
         if random.random() > epsilon:
             state = Variable(torch.FloatTensor(state), volatile=True)
-            print(state)
             q_value = self.forward(state)
-            print(q_value)
-            print(q_value.shape)
-            action = q_value.max(1)[1].data[0]
+            action = q_value.max(1)[1].view(1, 1)
         else:
             action = random.randrange(env.action_space.n)
         return action
@@ -105,9 +103,9 @@ def compute_loss(batch_size, done_episode):
     next_state = Variable(torch.FloatTensor(next_state))
     done = Variable(torch.FloatTensor(done))
 
-    q_values = DQN(state)
-    print(q_values.shape)
-    print(q_values.max(1)[0].shape)
+    action = action.unsqueeze(1)
+    q_values = DQN(state).gather(1, action)
+
 
 
     # print(action.shape)
@@ -116,12 +114,11 @@ def compute_loss(batch_size, done_episode):
     # q_values = q_values.gather(1, action)
 
     next_q_values = DQN(next_state)
-    print(next_q_values.shape)
 
     expected_q_value = reward + gamma*next_q_values.max(1)[0].detach()
-    print(expected_q_value.shape)
+    expected_q_value = expected_q_value.unsqueeze(1)
 
-    loss = F.smooth_l1_loss(q_values.max(1)[0], expected_q_value)
+    loss = F.smooth_l1_loss(q_values, expected_q_value)
     if done_episode:
         expected_q_value = reward
 
@@ -158,15 +155,10 @@ for i in range(total_episodes):
     epsilon = explore_stop + (explore_start - explore_stop) * np.exp(-decay_rate * decay_step)
     state = env.reset()
     state = state.transpose((2, 0, 1))
-    print("State size before stacking: ", state.shape)
     state, stacked_frames = stack_images(stacked_frames, state, True)
-    print("State size after stacking: ", state.shape)
     action = DQN.act(state, epsilon)
-    print("Action size: ", action)
     next_state, reward, done, _ = env.step(action)
-    print("Next state size before stacking: ", next_state.shape)
     next_state, stacked_frames = stack_images(stacked_frames, next_state.transpose((2, 0, 1)), True)
-    print("Next state size after stacking: ", next_state.shape)
     state = next_state
 
     while step < total_steps:
@@ -175,6 +167,7 @@ for i in range(total_episodes):
         epsilon = explore_stop + (explore_start - explore_stop) * np.exp(-decay_rate * decay_step)
 
         action = DQN.act(state, epsilon)
+        print(action)
         next_state, reward, done, _ = env.step(action)
 
         next_state, stacked_frames = stack_images(stacked_frames, next_state.transpose((2, 0, 1)), False)
@@ -182,6 +175,7 @@ for i in range(total_episodes):
 
         if done:
             total_steps = steps
+
         replay_buffer.push(state, action, reward, next_state, done)
         episode_rewards.append(reward)
         state = next_state
