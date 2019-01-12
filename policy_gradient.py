db4d6a3c741ad7fad311dc7847187a540b50caf8
SharathRaparthy <sharathchandra.raparthy@gmail.com>
2019-01-12 22:09:28 +0530

    policy conv

--- policy_gradient.py
+++ policy_gradient.py
@@ -2,12 +2,28 @@ import torch
 import torch.nn as nn
 from torch.distributions import Categorical
 import torch.optim as optim
+from skimage.color import rgb2gray
+from skimage import transform
+import torch.nn.functional as F
 import gym
 from torch.autograd import Variable
-env = gym.make('CartPole-v0')
+import numpy as np
+
+
+
+
+env = gym.make('Pong-v0')
 env.seed(1); torch.manual_seed(1);
 #build policy network
+def data_preprocess(image_frames):
 
+    normalize_image = image_frames/255.0
+    preprocessed_frame = transform.resize(normalize_image, [84,84], mode = 'reflect')
+    preprocessed_frame = np.transpose(preprocessed_frame, (2, 1, 0))
+    preprocessed_frame = np.expand_dims(preprocessed_frame, axis = 0)
+
+
+    return preprocessed_frame
 class PolicyNet(nn.Module):
     """docstring forPolicyNet."""
     def __init__(self):
@@ -25,17 +41,45 @@ class PolicyNet(nn.Module):
         model = nn.Sequential(self.input, nn.ReLU(),
                           self.output, nn.Softmax(dim=-1))
         return model(x)
+class PolicyConvNet(nn.Module):
+    """docstring for PolicyConvNet."""
+    def __init__(self):
+        super(PolicyConvNet, self).__init__()
+        self.action_size = env.action_space.n
+        self.conv1 = nn.Conv2d(3,32,8,stride = 4)
+        self.conv2 = nn.Conv2d(32,64,4,stride = 2)
+        self.conv3 = nn.Conv2d(64,64,3,stride = 1)
+        self.fc1 = nn.Linear(7*7*64,512)
+        self.fc2 = nn.Linear(512, self.action_size)
+        self.dropout = nn.Dropout(0.2)
+        self.reward_history = []
+        self.policy_history = []
+    def forward(self,x):
+        x = (F.relu(self.conv1(x)))
+
+        x = (F.relu(self.conv2(x)))
+        x = (F.relu(self.conv3(x)))
+
+        x = x.view(x.size(0),-1)
+        x = F.relu(self.fc1(x))
+        x = F.softmax(self.fc2(x),dim=-1)
+        # model = nn.Sequential(self.conv1, nn.ReLU(),self.conv2, nn.ReLU(),self.conv3, nn.ReLU(),self.fc1, nn.ReLU(),
+        #                       self.fc2, nn.Softmax(dim=-1))
+        return x
+
 policy_net = PolicyNet()
-print(policy_net)
 learning_rate = 0.01
 optimizer = optim.Adam(policy_net.parameters(), lr = learning_rate)
+#initialize policy conv net
+policy_conv_net = PolicyConvNet()
+optimizer_conv = optim.Adam(policy_conv_net.parameters(), lr = learning_rate)
 
 def pg_select_action(state):
     state = torch.from_numpy(state).type(torch.FloatTensor)
-    state = policy_net(Variable(state))
+    state = policy_conv_net(Variable(state))
     c = Categorical(state)
     action = c.sample()
-    policy_net.policy_history.append(c.log_prob(action))
+    policy_conv_net.policy_history.append(c.log_prob(action))
 
     return action
 def main():
@@ -43,13 +87,16 @@ def main():
     running_reward = 10
 
     for episodes in range(number_of_episodes):
+        state = env.reset()
+
         for time in range(1000):
-            state = env.reset()
+            state = data_preprocess(state)
+
 
             action = pg_select_action(state)
             #print(action)
             next_state, reward, done, _ = env.step(action.numpy().astype(int))
-            policy_net.reward_history.append(reward)
+            policy_conv_net.reward_history.append(reward)
             state = next_state
             if done:
                 break
@@ -58,7 +105,7 @@ def main():
         R = 0
         gamma = 0.99
         reward = []
-        for rewards in policy_net.reward_history[::-1]:
+        for rewards in policy_conv_net.reward_history[::-1]:
             R = rewards + gamma*R
             reward.insert(0,R)
 
@@ -67,7 +114,7 @@ def main():
         std = torch.std(reward)
         reward = (reward - means)/std
 
-        policies = torch.FloatTensor(policy_net.policy_history)
+        policies = torch.FloatTensor(policy_conv_net.policy_history)
         optimizer.zero_grad()
         loss = (torch.sum(torch.mul(policies,Variable(reward, requires_grad=True)).mul(-1),-1))
         loss.backward()
