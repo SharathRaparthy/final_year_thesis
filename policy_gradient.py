3a86956a8a22e761cc8aa058c2bb0eb22ef18fc6
Akshay <akshayk.vnit@gmail.com>
2019-01-13 01:03:25 +0530

    policy added

--- policy_gradient.py
+++ policy_gradient.py
@@ -8,22 +8,46 @@ import torch.nn.functional as F
 import gym
 from torch.autograd import Variable
 import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm, trange
+import pandas as pd
+from itertools import count
+from collections import deque
 
 
-
-
-env = gym.make('Pong-v0')
+env = gym.make('Breakout-v0')
 env.seed(1); torch.manual_seed(1);
 #build policy network
 def data_preprocess(image_frames):
+    image_frames = rgb2gray(image_frames)
 
     normalize_image = image_frames/255.0
     preprocessed_frame = transform.resize(normalize_image, [84,84], mode = 'reflect')
-    preprocessed_frame = np.transpose(preprocessed_frame, (2, 1, 0))
-    preprocessed_frame = np.expand_dims(preprocessed_frame, axis = 0)
+
 
 
     return preprocessed_frame
+stack_size = 4
+stacked_frames  =  deque([np.zeros((84,84), dtype=np.int) for i in range(stack_size)], maxlen=4)
+#returns stacked_frames-->Type: <type 'collections.deque'>  Size: (84,84,4)
+
+#stacked_state --> Type: np.ndarray                 Size: (84,84,4)
+def stack_images(stacked_frames, state, new_episode):
+    frame = data_preprocess(state)
+    if new_episode:
+        stacked_frames = deque([torch.zeros(state.shape) for i in range(stack_size)], maxlen=4)
+
+        stacked_frames.append(frame)
+        stacked_frames.append(frame)
+        stacked_frames.append(frame)
+        stacked_frames.append(frame)
+
+        stacked_state = np.stack(stacked_frames, axis = 0)
+        #print(stacked_state.shape)
+    else:
+        stacked_frames.append(frame)
+        stacked_state = np.stack(stacked_frames, axis = 0)
+    return stacked_state, stacked_frames
 class PolicyNet(nn.Module):
     """docstring forPolicyNet."""
     def __init__(self):
@@ -46,7 +70,7 @@ class PolicyConvNet(nn.Module):
     def __init__(self):
         super(PolicyConvNet, self).__init__()
         self.action_size = env.action_space.n
-        self.conv1 = nn.Conv2d(3,32,8,stride = 4)
+        self.conv1 = nn.Conv2d(4,32,8,stride = 4)
         self.conv2 = nn.Conv2d(32,64,4,stride = 2)
         self.conv3 = nn.Conv2d(64,64,3,stride = 1)
         self.fc1 = nn.Linear(7*7*64,512)
@@ -71,12 +95,27 @@ policy_net = PolicyNet()
 learning_rate = 0.01
 optimizer = optim.Adam(policy_net.parameters(), lr = learning_rate)
 #initialize policy conv net
-policy_conv_net = PolicyConvNet()
+policy_conv_net = PolicyConvNet().cuda()
 optimizer_conv = optim.Adam(policy_conv_net.parameters(), lr = learning_rate)
+episode_durations = []
+def plot_durations():
+    plt.figure(2)
+    plt.clf()
+    durations_t = torch.FloatTensor(episode_durations)
+    plt.title('Training...')
+    plt.xlabel('Episode')
+    plt.ylabel('Duration')
+    plt.plot(durations_t.numpy())
+    # Take 100 episode averages and plot them too
+    if len(durations_t) >= 100:
+        means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
+        means = torch.cat((torch.zeros(99), means))
+        plt.plot(means.numpy())
 
+    plt.pause(0.001)
 def pg_select_action(state):
     state = torch.from_numpy(state).type(torch.FloatTensor)
-    state = policy_conv_net(Variable(state))
+    state = policy_conv_net(Variable(state.cuda()))
     c = Categorical(state)
     action = c.sample()
     policy_conv_net.policy_history.append(c.log_prob(action))
@@ -85,21 +124,31 @@ def pg_select_action(state):
 def main():
     number_of_episodes = 10000
     running_reward = 10
+    stacked_frames  =  deque([np.zeros((84,84), dtype=np.int) for i in range(stack_size)], maxlen=4)
+
 
     for episodes in range(number_of_episodes):
         state = env.reset()
-
+        stacked_state, stacked_frames = stack_images(stacked_frames, state, True)
         for time in range(1000):
-            state = data_preprocess(state)
+            env.render()
+            stacked_state, stacked_frames = stack_images(stacked_frames, state, False)
 
+            state = np.expand_dims(stacked_state, axis=0)
 
             action = pg_select_action(state)
+            action = action.cpu()
             #print(action)
             next_state, reward, done, _ = env.step(action.numpy().astype(int))
             policy_conv_net.reward_history.append(reward)
             state = next_state
             if done:
+                episode_durations.append(time + 1)
+                plot_durations()
                 break
+
+
+
     #calculate the loss and update the weights
     #calculate the discounted rewards
         R = 0
@@ -115,11 +164,14 @@ def main():
         reward = (reward - means)/std
 
         policies = torch.FloatTensor(policy_conv_net.policy_history)
-        optimizer.zero_grad()
+        optimizer_conv.zero_grad()
         loss = (torch.sum(torch.mul(policies,Variable(reward, requires_grad=True)).mul(-1),-1))
         loss.backward()
-        optimizer.step()
+        optimizer_conv.step()
+
         running_reward = (running_reward * 0.99) + (time * 0.01)
+        policy_conv_net.reward_history = []
+        policy_conv_net.policy_history = []
         if episodes % 50 == 0:
             print('Episode {}\tLast length: {:5d}\tAverage length: {:.2f}'.format(episodes, time, running_reward))
 main()
